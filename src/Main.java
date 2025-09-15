6dd6aa979abe1f3817832c365bc5b7d48615383b
NadiiaOn <nadiia.onopriienko@gmail.com>
2025-09-15 22:50:18 +0200

    träna

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I managed to do it! Hoppas igen och det är funkar    ");
+        System.out.println("I managed to do it! Hoppas igen igen och det är funkar    ");
     }
 }
