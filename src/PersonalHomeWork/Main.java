7831fbed0990c4ee5318e2b3455aee5b2596f587
NadiiaOn <nadiia.onopriienko@gmail.com>
2025-09-18 20:59:20 +0200

    minor changes

--- src/PersonalHomeWork/Main.java
+++ src/PersonalHomeWork/Main.java
@@ -7,66 +7,51 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println(" =============== Hello! ==============  ");
-        System.out.println();
-        System.out.println("I am your personal-finance application!");
-        System.out.println();
+        System.out.println("\n I am your personal-finance application!");
 
-        Scanner scanner = new Scanner(System.in);
+      /*  Scanner scanner = new Scanner(System.in);
         System.out.print("What is your full name? ");
-
         String name = scanner.nextLine();
+
         if (name != null && !name.trim().isEmpty()) {
-            System.out.println();
-            System.out.println(" Hi " + name + "! \n I am happy to meet you!");
+            System.out.println("\n Hi, " + name + "! \n I am happy to meet you!");
         } else {
-            System.out.println();
-            System.out.println("You need to write your name...");
+            System.out.println("\n You need to write your name...");
         }
 
         Scanner scanner2 = new Scanner(System.in);
-        System.out.println();
-        System.out.print("What is your phone number? ");
+        System.out.print("\n What is your phone number? ");
 
         Long phoneNumber = scanner2.nextLong();
         if (phoneNumber > 0) {
-            System.out.println();
-            System.out.println(" This is your number " + phoneNumber);
+            System.out.println("\n This is your number " + phoneNumber);
         } else {
-            System.out.println();
-            System.out.println("You need to write your phone number");
+            System.out.println("\n You need to write your phone number");
         }
 
         Scanner scanner3 = new Scanner(System.in);
-        System.out.println();
-        System.out.print("How old are you? ");
-
+        System.out.print("\n How old are you? ");
         Byte age = scanner3.nextByte();
         if (age >= 18 && age <= 120) {
-            System.out.println();
-            System.out.println("You have the legal right to manage your account. ");
+            System.out.println("\n You have the legal right to manage your account. ");
         } else {
-            System.out.println();
-            System.out.println("You need to write your age");
+            System.out.println("\n You need to write your age");
         }
 
         Scanner scanner4 = new Scanner(System.in);
-        System.out.println();
-        System.out.print("Please, write your email address?");
-
+        System.out.print("\n Please, write your email address?");
         String emailOfHolder = scanner4.nextLine();
+
         if (emailOfHolder.contains("@")) {
-            System.out.println();
-            System.out.println("Thank you for the information!");
+            System.out.println("\n Thank you for the information!");
         } else {
-            System.out.println();
-            System.out.println("You need to write your email address");
+            System.out.println("\n You need to write your email address");
         }
-        System.out.println("What do you want to do further?");
 
         Scanner keyboard = new Scanner(System.in);
         System.out.println("""
-                "Choose the number for further actions:
-                1. Make a new account.
+                \n Choose the number for further actions:
+                1.Make a new account.
                 2. Change information in a current account.
                 3. Take the information from the current account.
                 4. Put the money into your account.
@@ -95,8 +80,13 @@ public class Main {
                 System.out.println("Other action with the account.");
                 break;
         }
-        var accountHolder = new AccountHolder(name, phoneNumber, age, 0, "my address", emailOfHolder);
+        det funkar
+       */
+
+       /* var accountHolder = new AccountHolder(name, phoneNumber, age, 0, "my address", emailOfHolder);
         accountHolder.personalAddress();
         System.out.println(accountHolder.getAge());
+        exempel för mig
+        */
     }
 }
